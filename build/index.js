36cccb20c08ecf57dadd86759d122a3ab1cc3f02
EfiokobongN <mailpreciousnelson@gmail.com>
2024-11-27 09:22:45 +0000

    my first typescript code and setup

---
+++ build/index.js
@@ -0,0 +1,8 @@
+"use strict";
+let project = 5;
+let drive = 'Slow';
+let nothing = null;
+let notExist = undefined;
+let now = new Date();
+let colors = ['green', 'yellow', 'black', 'red', 99, 20];
+let threeDimension = [[['one', 'two', 'three']]]; //array of three level
---
+++ src/test.ts
@@ -0,0 +1,12 @@
+let project = 5;
+let drive: string = 'Slow';
+
+let nothing = null;
+let notExist = undefined;
+
+
+let now = new Date();
+
+let colors = ['green', 'yellow', 'black', 'red', 99, 20];
+
+let threeDimension: string[][][] = [[['one', 'two', 'three']]]; //array of three level
