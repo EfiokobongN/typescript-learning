c3479c1b3fba093d9073d588766281e30c7d59ef
EfiokobongN <mailpreciousnelson@gmail.com>
2024-11-27 11:54:51 +0000

    first typpescript function

---
+++ src/function.ts
@@ -0,0 +1,12 @@
+//Typescript Functions
+
+const add = (a:number, b:number):number =>{
+    return a * b;
+}
+
+console.log(add(4,5));
+
+
+const logger = (message:string):void =>{
+    console.log(message);
+}
