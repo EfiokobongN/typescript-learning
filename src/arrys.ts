7b6aa2a8cd270446e9a38d5790b8d9ca6b79a6c9
EfiokobongN <mailpreciousnelson@gmail.com>
2024-11-27 12:05:18 +0000

    array of string and number

--- src/arrys.ts
+++ src/arrys.ts
@@ -1,8 +1,9 @@
-"use strict";
 // Typescript Array
-let skills = [];
+
+let skills: (number | string)[] = [];
 skills[0] = "problem solving";
-skills[1] = "programming";
+skills[1]= "programming";
 skills.push("Software Development");
 skills.push(4000);
+
 console.table(skills);
