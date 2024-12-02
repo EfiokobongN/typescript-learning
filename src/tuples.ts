72e90f8e352133c059a891c6fb5bba26cc72f4c5
EfiokobongN <mailpreciousnelson@gmail.com>
2024-12-02 16:00:28 +0000

    Tuples

---
+++ src/tuples.ts
@@ -0,0 +1,12 @@
+// Tuples
+
+//const denise = {
+   // eyes: 'brown',
+   // experience: true,
+    //level: 3,
+    //visited: Date(),
+//}
+type Student = [string, boolean, number, Date ]
+let frank : Student =['brown', true, 3, new Date()]
+
+console.log(frank[0]);
